af05061983f21f16e5556c26841084617b804dc6
ChiHao <chihao@example.com>
2025-10-10 09:06:16 +0700

    Hehe

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,7 @@ const ExcelJS = require('exceljs');
 const fs = require('fs');
 
 const app = express();
-app.use(cors({
-  origin: '*'
-}));
-
-
-
+app.use(cors());
 
 // Tăng giới hạn body request lên 100MB
 app.use(express.json({ limit: '100mb' }));
@@ -26,27 +21,11 @@ app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use('/uploads', express.static('uploads'));
 
 // ✅ Kết nối MySQL dùng POOL (ổn định cho Railway, chống tự đóng kết nối)
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || 'localhost',
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || '48194007',
-  database: process.env.MYSQLDATABASE || 'warehouse_db',
-  port: process.env.MYSQLPORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10, // Railway mỗi lần cấp khoảng 10 kết nối
-  queueLimit: 0
-});
-
-// Dùng pool.promise() để xài async/await mượt mà
-const db = pool.promise();
-
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error('❌ Lỗi kết nối MySQL (POOL):', err);
-  } else {
-    console.log('✅ Kết nối MySQL (POOL) thành công');
-    connection.release(); // giải phóng kết nối về pool
-  }
+const db = mysql.createConnection({ 
+  host: 'localhost', 
+  user: 'root', 
+  password: '48194007', 
+  database: 'warehouse_db' 
 });
 
 
